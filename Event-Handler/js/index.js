aaab9b0356aee503d3fdd3e38f556d16f4566271
StevenSR4 <stevenfigo04@gmail.com>
2021-04-01 22:06:00 +0800

    added index.html

--- Event-Handler/js/index.js
+++ Event-Handler/js/index.js
@@ -3,7 +3,8 @@ function transformUppercase(_val){
 }
 
 //event listener
+
 var elnamaPengguna= document.getElementById ("namaPengguna"); // select elemet
 elnamaPengguna.addEventListener("keyup",function(){
-elnamaPengguna.value= transformUppercase(elnamaPengguna.value)
+elnamaPengguna.value= transformUppercase(elnamaPengguna.value);
 });
