10685a68f8fcde5f61d06396ecf6045c774e2d48
StevenSR4 <stevenfigo04@gmail.com>
2021-04-02 19:41:37 +0800

    added 1941

--- Event-Handler/js/index.js
+++ Event-Handler/js/index.js
@@ -8,3 +8,17 @@ var elnamaPengguna= document.getElementById ("namaPengguna"); // select elemet
 elnamaPengguna.addEventListener("keyup",function(){
 elnamaPengguna.value= transformUppercase(elnamaPengguna.value);
 });
+
+function checkPassword(){
+    let elkataLaluan = document.getElemetById ("kataLaluan");
+    let password_val = elkataLalauan.value;
+    if (password_val.length>5){
+        alert("password check...ok !");
+        else {
+            alert ("password too short");
+        }
+    }
+}
+
+let elkataLaluan = document.getElementById("kataLaluan");
+elkataLalauan.onblur = checkPassword; 
