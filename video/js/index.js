fa56a9c8bfd4bbd03469e0489f4c1fbaeaf8a53f
StevenSR4 <stevenfigo04@gmail.com>
2021-04-05 21:14:07 +0800

    2114

--- video/js/index.js
+++ video/js/index.js
@@ -24,7 +24,7 @@ seekBar.addEventListener("change", function(){
     let time = video.duration * (seekBar.value/100);
     video.currentTime= time;
 });
-video.addEventListener("timeUpdate", function(){
+video.addEventListener("timeupdate", function(){
     let value = (100/video.duration) * video.currentTime;
     seekBar.value = value;
 
