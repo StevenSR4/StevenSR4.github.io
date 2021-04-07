f4b534a248e860ed7091111a77c1ebb887bb5fe1
StevenSR4 <stevenfigo04@gmail.com>
2021-04-07 20:39:56 +0800

    2039

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -8,5 +8,8 @@ let app2 = new vue ({
     el:"app2",
     data: {
         message: "Loaded on "+ new Date().toLocaleDateString();
+       linkUrl: "https://bbc.in/2LanJs8"
+       linkText: "Google is down!"
     }
+
 })
