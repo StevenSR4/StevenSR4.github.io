7156824a173d752fcb303e7d15d22312749d03a7
StevenSR4 <stevenfigo04@gmail.com>
2021-04-07 20:51:59 +0800

    2051

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -5,11 +5,38 @@ let app = new vue({
     }
 }); 
 let app2 = new vue ({
-    el:"app2",
+    el:"#app2",
     data: {
         message: "Loaded on "+ new Date().toLocaleDateString();
        linkUrl: "https://bbc.in/2LanJs8"
        linkText: "Google is down!"
     }
 
-})
+});
+
+let app3 =new vue({
+    el: "#app3",
+    data: {
+
+        showIt:false
+    }
+});
+
+let app4=new vue({
+    el: "#app4",
+    data: {
+        theList: [
+            {
+                text:"Learn HTML"
+            },
+            {
+                text: "Learn CSS"
+
+            },
+            {
+                text: "Learn JavaScript"
+
+            }
+        ]
+    }
+});
