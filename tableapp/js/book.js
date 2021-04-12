dbddae46120fc0b00873d4be57a9a17f0cd6989d
StevenSR4 <stevenfigo04@gmail.com>
2021-04-12 19:43:16 +0800

    1943

---
+++ tableapp/js/book.js
@@ -0,0 +1,19 @@
+let bookNowBtn = document.getElementById("bookNow");
+bookNowBtn.addEventListener("click", function(){
+    let userName = document.getElementById("userName");
+    let userNameVal = userName.nodeValue;
+
+    letuserEmail= document.getElementById("userEmail");
+    let userEmail = userEmail.value;
+
+
+    let userPaxVal=document.getElementById ("userPax").value;
+    let userRemarksVal = document.getElementById("userremarks").value;
+
+    BookNow (userNameVal, userEmailVal, userPaxVal,userRemarksVal);
+
+});
+
+function BookNow (userName, userEmail, userPax,userRemarks){
+    alert(userName);
+}
