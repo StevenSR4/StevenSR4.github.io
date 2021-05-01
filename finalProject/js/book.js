29a246cbc49c8a9c80c39fb4a81113b57d9e0605
StevenSR4 <stevenfigo04@gmail.com>
2021-05-01 22:15:24 +0800

    2215

--- finalProject/js/book.js
+++ finalProject/js/book.js
@@ -1,41 +1,41 @@
 let bookNowBtn = document.getElementById("bookNow");
-bookNowBtn.addEventListener("click", function(){
+bookNowBtn.addEventListener("click", function () {
 
-    let userNameVal = document.getElementById("userName").value;
-    let userEmailVal= document.getElementById("userEmail").value;
-    let userTrainingVal=document.getElementById ("userTraining").value;
-    let userLevelVal=document.getElementById ("userLevel").value;
-    let userTimeVal=document.getElementById ("userTime").value;
-    
-    let userRemarksVal = document.getElementById("userRemarks").value;
+  let userNameVal = document.getElementById("userName").value;
+  let userEmailVal = document.getElementById("userEmail").value;
+  let userTrainingVal = document.getElementById("userTraining").value;
+  let userLevelVal = document.getElementById("userLevel").value;
+  let userTimeVal = document.getElementById("userTime").value;
 
-    BookNow (userNameVal, userEmailVal, userTrainingVal, userLevelVal, userTimeVal,userRemarksVal);
+  let userRemarksVal = document.getElementById("userRemarks").value;
+
+  BookNow(userNameVal, userEmailVal, userTrainingVal, userLevelVal, userTimeVal, userRemarksVal);
 
 });
 
-function BookNow (userName, userEmail, userTraining, userLevel, userTime,userRemarks){
-    
-    let url = 'https://api.sheety.co/9192796fbfeb32fc266d9e164e704c37/finalproject/bookingitems';
-    let body = {
-      bookingitem: {
-        name: userName,
-        email: userEmail,
-        training: userTraining,
-        level: userLevel,
-        time: userTime,
-        remarks: userRemarks
+function BookNow(userName, userEmail, userTraining, userLevel, userTime, userRemarks) {
+
+  let url = 'https://api.sheety.co/9192796fbfeb32fc266d9e164e704c37/finalproject/bookingitems';
+  let body = {
+    bookingitem: {
+      name: userName,
+      email: userEmail,
+      training: userTraining,
+      level: userLevel,
+      time: userTime,
+      remarks: userRemarks
     }
   }
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers:{
-          "Content-Type": "application/json"
-      }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json"
+    }
 
-    })
+  })
     .then((response) => response.json())
     .then(json => {
-      alert("ID:"+ json.bookingitem.id +"," + json.bookingitem.name + "succesfully added!");
+      alert("ID:" + json.bookingitem.id + "," + json.bookingitem.name + "succesfully added!");
     });
-}  
+}
